d34c61a6c5a6e0c2654eb2b76e76ce178146a2b2
liqiang <liqiang@orvibo.com>
2016-08-08 14:06:39 +0800

    不知道是什么情况

--- PHP/class/upload.php
+++ PHP/class/upload.php
@@ -1,8 +1,61 @@
+
+
+<html>
+<head>
+    <title>Uploding...</title>
+</head>
+<body>
+<h1>Uploading file...</h1>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: liqiang
- * Date: 16/7/8
- * Time: 11:15
- */
-echo "ce shi git ssss";
+
+if($_FILES['userfile']['error'] > 0)  {
+    echo "Problem:.<br>";
+    switch ($_FILES['userfile']['error']) {
+        case 1:
+            echo 'File exceeded upload_max_filesize';
+            break;
+        case 2:
+            echo 'File exceeded max_file_size';
+            break;
+        case 3:
+            echo 'File only parially upload';
+            break;
+        case 4:
+            echo 'No file upload';
+            break;
+        case 6:
+            echo 'Cannot upload file: No temp directory specified';
+            break;
+        case 7:
+            echo 'Upload failed: Cannot write to disk';
+            break;
+    }
+    exit;
+}
+$upfile = '/uploads/'.$_FILES['userfile']['name'];
+echo $upfile;
+if($_FILES['userfile']['type'] != 'text/plain') {
+    echo 'Problem: file is not plain text';
+    exit;
+}
+
+
+exit;
+if(is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    if (!move_uploaded_file($_FILES['userfile']['tmp_name'],$upfile)) {
+        echo 'Problem: Could not move file to destination directory';
+        exit;
+    }
+}else {
+    echo 'Problem:Possible file upload attact.Filename';
+    echo $_FILES['userfile']['name'];
+    exit;
+}
+echo "File upload successfully.<br>.<br>";
+
+?>
+
+</body>
+
+</html>
